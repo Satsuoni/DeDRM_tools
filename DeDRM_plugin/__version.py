73a608ab086f35927426220a887d226d346daa89
Satsuoni <satsuoni@hotmail.com>
2023-06-24 22:41:37 +0900

    Adjusted version number

--- DeDRM_plugin/__version.py
+++ DeDRM_plugin/__version.py
@@ -4,7 +4,7 @@
 #@@CALIBRE_COMPAT_CODE@@
 
 PLUGIN_NAME = "DeDRM"
-__version__ = '10.0.5s'
+__version__ = '10.0.5'
 
 PLUGIN_VERSION_TUPLE = tuple([int(x) for x in __version__.split(".")])
 PLUGIN_VERSION = ".".join([str(x)for x in PLUGIN_VERSION_TUPLE])
